ea76517af541b70b46e4ca7b8460bcc61167abca
Rupin <rupin.chheda@gmail.com>
2019-10-01 17:05:25 +0530

    remove Google Refs

--- userprofile/utils/renderPDF.py
+++ userprofile/utils/renderPDF.py
@@ -7,9 +7,9 @@ import base64
 def render_to_pdf(template_src, context_dict={}):
     template = get_template(template_src)
     html  = template.render(context_dict)
-    result = open('abc.pdf', 'wb')
-    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
-    result.close()
+    # result = open('abc.pdf', 'wb')
+    # pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    # result.close()
     result = BytesIO()
     pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
     return base64.b64encode(result.getvalue()).decode()
